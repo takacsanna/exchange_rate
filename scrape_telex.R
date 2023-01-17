06959d750f1641012e3562099a7a806cbb2eed7f
takacsanna <t.anna0109@gmail.com>
2023-01-17 14:23:08 +0100

    scraping (multiple pages of content)

--- scrape_telex.R
+++ scrape_telex.R
@@ -1,38 +1,30 @@
 library(tidyverse)
 library(rvest)
 
-page <- read_html("https://telex.hu")
+url_start <- "https://telex.hu/legfrissebb?oldal="
+#5102
+url_ending <- 1:10
+telex_add_df <- tibble(url = str_c(url_start, url_ending))
 
-#címsorok kiválasztása
-my_node <- page %>% 
-  html_nodes(".item__title")
-
-#címek szöveggé alakítása
-cim <- my_node %>% 
-  html_text() %>% 
-  as_data_frame(.) %>% 
+telex_add_df <- telex_add_df %>% 
   mutate(
-    id = row_number()
+    page = map(url, read_html),
+    nodes = map(page, ~ html_nodes(., ".list__item__title")),
+    cim = map(nodes, html_text),
+    url_to_cim = map(nodes, html_attr, "href")
   )
 
-#url-ek
-url <- my_node %>% 
-  html_attr("href") %>% 
-  as_data_frame(.) %>% 
-  mutate(
-    id = row_number()
-  )
-df <- merge(cim, url, by = "id") %>% 
-  na.omit() %>% 
-  unique() %>% 
-  set_names("id", "cim", "url")
+telex_add_df <- telex_add_df %>% 
+  select(url_to_cim, cim) %>% 
+  unnest() %>% 
+  na.omit() %>%
+  unique() 
 
-df<- df %>% 
+telex_add_df<- telex_add_df %>% 
   mutate(
-    url2 = ifelse(substr(url,1,1) == "/", str_c("https://telex.hu", url), url)
+    url_to_cim = ifelse(substr(url_to_cim,1,1) == "/", str_c("https://telex.hu", url_to_cim), url_to_cim)
   )
 
-df
 
 cikkek <- function(link) {
   link %>% 
@@ -45,17 +37,15 @@ cikkek <- function(link) {
 
 cikkek("https://telex.hu/gazdasag/2023/01/14/csaladtamogatas-csok-falusi-otthonfelujitas-babavaro-anya-gyerek")
 
-df<-df %>% 
+telex_add_df<-telex_add_df %>% 
   mutate(
-    szoveg = map(url2, cikkek)
+    szoveg = map(url_to_cim, cikkek)
   )
-
-df
 
 proba <- read_html("https://telex.hu/gasztro/2023/01/17/energiavalsag-etterem-vendeglatas-csod-bezaras-inflacio") %>% 
   html_nodes(".history--original span") %>% 
   html_text() %>% 
-  str_extract("2023. január \\d\\d. – \\d\\d:\\d\\d")
+  str_extract("202\\d. .* \\d\\d. – \\d\\d:\\d\\d")
 proba
 
 datum <- function(link) {
@@ -63,25 +53,27 @@ datum <- function(link) {
     read_html() %>% 
     html_nodes(".history--original span") %>% 
     html_text() %>% 
-    str_extract("2023. január \\d\\d. – \\d\\d:\\d\\d")
+    str_extract("202\\d. .* \\d\\d. – \\d\\d:\\d\\d")
 }
 
 datum("https://telex.hu/szorakozas/2023/01/17/marcius-1-mandalorian-uj-evada-star-wars-disney-elozetes")
 
-df<-df %>% 
+telex_add_df<-telex_add_df %>% 
   mutate(
-    date = map(url2, datum)
-  ) %>% 
-  set_names("id", "cim", "url", "url2", "szoveg", "date")
+    date = map(url_to_cim, datum)
+  )
 
-df <- as_data_frame(df)
-df$szoveg <- as.character(df$szoveg)
-df$date <- as.character(df$date)
+telex_add_df <- as_data_frame(telex_add_df)
+telex_add_df$szoveg <- as.character(telex_add_df$szoveg)
+telex_add_df$date <- as.character(telex_add_df$date)
 
-df<-df %>% 
-  drop_na()
-df$cim <- iconv(df$cim,from="UTF-8",to="ASCII//TRANSLIT")
-df$szoveg <- iconv(df$szoveg,from="UTF-8",to="ASCII//TRANSLIT")
+telex_add_df<-telex_add_df %>% 
+  drop_na() %>% 
+  unique()
+
+telex_add_df$cim <- iconv(telex_add_df$cim,from="UTF-8",to="ASCII//TRANSLIT")
+telex_add_df$szoveg <- iconv(telex_add_df$szoveg,from="UTF-8",to="ASCII//TRANSLIT")
+telex_add_df$date <- iconv(telex_add_df$date,from="UTF-8",to="ASCII//TRANSLIT")
 
 df %>% 
   write_csv(file = str_c("telex_17.csv"))
