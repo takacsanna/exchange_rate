dd9f40354f77af1447caf63c33fe2b6d598ba910
takacsanna <t.anna0109@gmail.com>
2024-03-28 13:11:59 +0100

    updated new trading

--- 07-ar3.R
+++ 07-ar3.R
@@ -15,16 +15,7 @@ testing_set <- exchage_text_df |>
 
 
 training_ts <- xts::xts(training_set$price, training_set$time)
-attr(training_ts, "frequency") <- 60
-training_ts <- as.ts(training_ts)
-
-teljes_ts <- xts::xts(exchage_text_df$price, exchage_text_df$time)
-attr(teljes_ts, "frequency") <- 60
-teljes_ts <- as.ts(teljes_ts)
-
 test_ts <- xts::xts(testing_set$price, testing_set$time)
-attr(test_ts, "frequency") <- 60
-test_ts <- as.ts(test_ts)
 
 fit <- auto.arima(training_ts)
 summary(fit)
@@ -39,13 +30,12 @@ prediction_df %>%
   ggplot(aes(x=time, y=resid)) + 
   geom_line()
 
-testing_set$szazalek <- testing_set$price / lag(testing_set$price)
-ggplot(testing_set, aes(x=time, y=szazalek))+geom_line()
-
+testing_set$percent <- testing_set$price / lag(testing_set$price)
+ggplot(testing_set, aes(time, percent))+geom_line()
 
 balance_df <- tibble(balance = 1000000, stock = T)
-l_bound = 0.9975
-u_bound = 1.0025
+l_bound = 0.99975
+u_bound = 1.00025
 
 for (i in 2:(nrow(prediction_df)-1)) {
   # iteration for calculating the balance
@@ -73,3 +63,4 @@ for (i in 2:(nrow(prediction_df)-1)) {
 }
 balance_df$index <- 1:nrow(balance_df)  
 ggplot(balance_df, aes(x=index, y=balance))+geom_line()
+
